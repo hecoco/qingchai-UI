6aa74cfd72e343375851af9281f1833aa03aecb8
佩岭 <2534856130@qq.com>
2022-04-04 18:55:54 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Chao />
+</template>
+
+<script>
+import Chao from '@/components/Chao.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Chao
+  }
+}
+</script>
---
+++ src/components/Chao.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        chaochao
+    </div>
+</template>
