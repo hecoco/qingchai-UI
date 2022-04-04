9a5a2f11d90bcb39ff419a4021b50dfbac5ba948
佩岭 <2534856130@qq.com>
2022-04-04 21:22:13 +0800

    doc css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
             <h1>青柴UI</h1>
             <h2>一个基于Vue3的UI框架</h2>
             <p class="actions">
-                <a>GitHub</a>
-                <a>开始</a>
+                <a href="http://github.com">GitHub</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
@@ -20,4 +20,25 @@ export default{
 </script>
 
 <style lang="scss" scoped>
+.banner{
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions{
+        padding:  8px 0;
+        a {
+            margin: 0 9px;
+            background: #fff;
+            display: inline-block;
+            $h:28px;
+            height: $h;
+            line-height: $h;
+            border-radius: 28px/2;
+            padding: 0 8px;
+        }
+    }
+}
 </style>
