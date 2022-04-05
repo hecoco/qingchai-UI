b4be53a4cbc06590d5aa272ef5a4fb24e69f587b
佩岭 <2534856130@qq.com>
2022-04-05 20:57:32 +0800

    基本完成button

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import './lib/qingchai.scss';
+import './index.scss';
 import { router } from './router';
 import { createApp } from 'vue';
 import App from './App.vue';
-import './lib/qingchai.scss';
-import './index.scss';
+
 
 const app = createApp(App);
 app.use(router);
