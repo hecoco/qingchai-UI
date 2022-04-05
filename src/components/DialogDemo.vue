3bcfe22d0418ed96b99e1d2bdbc0a8c3486da377
佩岭 <2534856130@qq.com>
2022-04-05 23:23:06 +0800

    dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    dialog
+    <Dialog
+      :visible="true"
+      title="标题"
+      @yes='fn1'
+      @no='fn2'
+    />
   </div>
 </template>
+
+<script lang='ts'>
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: { Dialog },
+  setup() {
+    const fn1 = () => {
+      console.log(1);
+    };
+    return { fn1 };
+  },
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>title</header>
+    <main>
+      <p>文字1文字1文字</p>
+      <p>文字2文字2文字</p>
+    </main>
+    <footer>
+      <Button>OK</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+<script>
+import Button from "./Button.vue";
+export default {
+  setup() {},
+  components: { Button },
+};
+</script>
