409bdc9919c666791498456f928ee933b7170e41
佩岭 <2534856130@qq.com>
2022-04-05 17:56:26 +0800

    button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
+  <Button @click="onClick">
     button
-  </div>
+  </Button>
 </template>
+
+<script lang='ts'>
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+  setup() {
+    const onClick = () => {};
+    return { onClick };
+  },
+};
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button v-bind="$attrs">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+  setup(props, context) {
+    const { size, ...rest } = context.attrs;
+    return { size, rest };
+  },
+};
+
+//#00A1D6正常  #00B5E5悬浮 #80D0Eb禁止 蓝色
+//#FB7299 #FC8BAB
+//#ffffff #E3E5E7
+</script>
