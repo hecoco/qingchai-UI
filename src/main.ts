f13bfa26fcc7ac5435ec77ecf4784660a9426cb7
佩岭 <2534856130@qq.com>
2022-04-08 00:55:30 +0800

    在次优化Markdown

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { router } from './router';
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'github-markdown-css';
-
+import Markdown from './components/Markdown.vue';
 
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+app.component('Markdown', Markdown);
