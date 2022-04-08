b0eff0aaddf7800016e21cc4f0263a89f9116e0d
佩岭 <2534856130@qq.com>
2022-04-09 02:10:58 +0800

    npm

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git add . &&
 git commit -m "update" &&
 git branch -M master &&
 git remote add origin git@github.com:hecoco/qingchai-UI-website.git &&
-git push -f -u origin master &&
+git push -f -u origin master:gh-pages &&
 cd -
